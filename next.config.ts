13131b5e874c5e765d8ed3fae3ed99b8306031e3
WilliamDocker <paucarkenneth8@gmail.com>
2025-03-12 18:20:43 -0500

    cambiosimagens

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
+  basePath: '/Zapatillas',
+  assetPrefix: '/Zapatillas/',
 };
 
 export default nextConfig;
