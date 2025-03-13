2b5bd7fffcdb6c3b9fe15dcd3bdd9f44772002f8
WilliamDocker <paucarkenneth8@gmail.com>
2025-03-12 19:14:01 -0500

    rutaabsoluta

--- src/components/ui/ProductCard.tsx
+++ src/components/ui/ProductCard.tsx
@@ -1,12 +1,13 @@
+import Image from "next/image"
 import style from "./card.module.css"
 
 const rutacards = [
-    { image: "/images/cards/Superstar.webp", name: "Botin Retro 35-42"},
-    { image: "/images/cards/Superstar.webp", name: "Puma 35-42"},
-    { image: "/images/cards/NEGROYBLANCO.webp", name: "Botin Retro 35-42"},
-    { image: "/images/cards/NeoAdidas.jpg", name: "Botin Retro 35-42"},
-    { image: "/images/cards/pUMA.jpg", name: "Botin Retro 35-42"},
-    { image: "/images/cards/Superstar.webp", name: "Botin Retro 35-42"},
+    { image: "https://personalproyect2.github.io/Zapatillas/images/cards/Superstar.webp", name: "Botin Retro 35-42"},
+    { image: "https://personalproyect2.github.io/Zapatillas/images/cards/Superstar.webp", name: "Puma 35-42"},
+    { image: "https://personalproyect2.github.io/Zapatillas/images/cards/NEGROYBLANCO.webp", name: "Botin Retro 35-42"},
+    { image: "https://personalproyect2.github.io/Zapatillas/images/cards/NeoAdidas.jpg", name: "Botin Retro 35-42"},
+    { image: "https://personalproyect2.github.io/Zapatillas/images/cards/pUMA.jpg", name: "Botin Retro 35-42"},
+    { image: "https://personalproyect2.github.io/Zapatillas/images/cards/Superstar.webp", name: "Botin Retro 35-42"},
 ];
 
 export default function Card(){
@@ -16,11 +17,14 @@ export default function Card(){
            
             {rutacards.map((card, index) => (
                 <div key={index} className={style.productcard}>
-                    <img
+                    <Image
                         className={style.productImage} 
+                        width={350} 
+                        height={350}    
+                        layout="fixed"
                         src={card.image} 
                         alt={card.name} 
-                        
+                        priority={true}
                     />
                     <h2 className={style.productcardh2}>{card.name}</h2>
                 </div>
